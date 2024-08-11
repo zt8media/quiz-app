8a02fd0fe341b41025663f772b7bffe520769a03
zt8media <zt8media@gmail.com>
2024-08-11 11:56:36 -0400

    Add Results Page with styling and routing
    - Created a Results page with title, results text, and button
    - Styled components for title, highlighted text, and button
    - Updated routing to include Results page

--- my-quiz-app/src/Pages/Quiz.jsx
+++ my-quiz-app/src/Pages/Quiz.jsx
@@ -6,14 +6,14 @@ import Footer from '../components/Footer';
 function Quiz() {
   return (
     <>
-      <Navbar />
+     
       <div>
         <h1>Quiz Page</h1>
         <p>Users should be presented with one question at a time and be able to select an answer.
 Users should be able to submit their answers 
 </p>
       </div>
-      <Footer />
+      
     </>
   );
 }
--- my-quiz-app/src/Pages/Results.jsx
+++ my-quiz-app/src/Pages/Results.jsx
@@ -1,20 +1,57 @@
-// src/pages/Results.jsx
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-function Results() {
+const ResultsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  color: #009688;
+  font-size: 5rem;
+  margin-bottom: 20px;
+`;
+
+const ResultsText = styled.p`
+  font-size: 1.5rem;
+  color: black;
+  margin: 10px 0;
+`;
+
+const Highlight = styled.span`
+  background-color: lightgrey;
+  color: black;
+  padding: 0 5px;
+  border-radius: 3px;
+`;
+
+const TryAgainButton = styled(Link)`
+  display: inline-block;
+  padding: 10px 20px;
+  font-size: 1.2rem;
+  color: white;
+  background-color: #009688;
+  text-decoration: none;
+  border-radius: 5px;
+  margin-top: 20px;
+
+  &:hover {
+    background-color: #00796b; /* Slightly darker shade for hover effect */
+  }
+`;
+
+const Results = () => {
   return (
-    <>
-      <Navbar />
-      <div>
-        <h1>Results Page</h1>
-        <p>Should be a static recreation of the current Results Page
-</p>
-      </div>
-      <Footer />
-    </>
+    <ResultsContainer>
+      <Title>lrnr</Title>
+      <ResultsText>Questions Right: <Highlight>0111</Highlight></ResultsText>
+      <TryAgainButton to="/quiz">Try Another Quiz</TryAgainButton>
+    </ResultsContainer>
   );
-}
+};
 
 export default Results;
