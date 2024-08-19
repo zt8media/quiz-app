7d27acd16c08ad68850b26a0c4489f2a55a2d84f
zt8media <zt8media@gmail.com>
2024-08-19 14:35:16 -0400

    update-url

--- my-quiz-app/src/Pages/Generate.jsx
+++ my-quiz-app/src/Pages/Generate.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://quiz-app-0ql9.onrender.com';
 function Generate() {
   const [topic, setTopic] = useState('golang'); // Defaulting quiz answer choices
   const [expertise, setExpertise] = useState('novice'); 
